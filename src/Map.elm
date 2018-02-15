57e186d4a4d030c4ae5a84fb39d2714c24bcbd66
Joël Quenneville <joelq@thoughtbot.com>
2018-02-15 00:26:49 -0500

    Remove Layer type
    Previously, the `Map` type had a list of `Layer` types. For the current
    map, I had a land layer and a sea layer. Since I always want only those
    two layers, I decided to make them explicit keys on the map instead. I
    also need access to the land layer for pathfinding.
    
    At that point, the `Layer` type was just extra abstraction for nothing
    so I removed it and inlined the lists of tiles in the `Map`.

--- src/Map.elm
+++ src/Map.elm
@@ -1,22 +1,283 @@
 module Map exposing (Map, render, level1)
 
-import Layer exposing (Layer)
 import Element exposing (Element)
+import Tile exposing (Tile(..))
+import TileSheet exposing (TileSheet)
+import List.Extra
 
 
 type alias Map =
-    { layers : List Layer
+    { land : List Tile
+    , sea : List Tile
+    , width : Int
+    , sheet : TileSheet
     }
 
 
 level1 : Map
 level1 =
-    { layers = [ Layer.sea, Layer.land ]
+    { land = landTiles
+    , sea = seaTiles
+    , sheet = TileSheet.kennyPirates
+    , width = 15
     }
 
 
 render : Map -> Element
 render map =
-    map.layers
-        |> List.map Layer.render
+    [ renderSea map, renderLand map ]
         |> Element.layers
+
+
+renderSea : Map -> Element
+renderSea { sheet, sea, width } =
+    renderTiles sheet sea width
+
+
+renderLand : Map -> Element
+renderLand { sheet, land, width } =
+    renderTiles sheet land width
+
+
+renderTiles : TileSheet -> List Tile -> Int -> Element
+renderTiles sheet tiles width =
+    tiles
+        |> List.map (Tile.render sheet)
+        |> List.Extra.groupsOf width
+        |> List.map (Element.flow Element.right)
+        |> Element.flow Element.down
+
+
+seaTiles : List Tile
+seaTiles =
+    List.map (always <| TileId 73) landTiles
+
+
+landTiles : List Tile
+landTiles =
+    [ TileId 23
+    , TileId 23
+    , TileId 24
+    , TileId 25
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 55
+    , TileId 55
+    , TileId 56
+    , TileId 55
+    , TileId 37
+    , TileId 23
+    , TileId 39
+    , TileId 23
+    , TileId 23
+    , TileId 36
+    , TileId 55
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 37
+    , TileId 24
+    , TileId 23
+    , TileId 36
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 7
+    , TileId 9
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 37
+    , TileId 24
+    , TileId 41
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 8
+    , TileId 53
+    , TileId 40
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 37
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 53
+    , TileId 23
+    , TileId 40
+    , TileId 24
+    , TileId 41
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 22
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 38
+    , TileId 40
+    , TileId 39
+    , TileId 39
+    , TileId 23
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 22
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 37
+    , TileId 23
+    , TileId 24
+    , TileId 40
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 22
+    , TileId 41
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 22
+    , TileId 24
+    , TileId 36
+    , TileId 56
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 55
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 1
+    , TileId 2
+    , TileId 3
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 7
+    , TileId 9
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 1
+    , TileId 2
+    , TileId 21
+    , TileId 4
+    , TileId 35
+    , NoTile
+    , NoTile
+    , TileId 22
+    , TileId 24
+    , TileId 25
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 1
+    , TileId 21
+    , TileId 4
+    , TileId 34
+    , TileId 35
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 54
+    , TileId 55
+    , TileId 57
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 33
+    , TileId 34
+    , TileId 35
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 9
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 53
+    , NoTile
+    , TileId 6
+    , TileId 7
+    , TileId 7
+    , TileId 7
+    , TileId 53
+    , TileId 52
+    , TileId 8
+    , TileId 9
+    , NoTile
+    , NoTile
+    , TileId 6
+    , TileId 7
+    , TileId 53
+    , TileId 40
+    ]
